94ac2d8657a21fdc07d01f85e0e81e2ebfb8eae1
YuriSegal <yurisegal8@gmail.com>
2022-10-28 17:28:09 +0300

    initial code

---
+++ src/java_introduction/BitOperations.java
@@ -0,0 +1,40 @@
+package java_introduction;
+
+/**
+ * right nBit value [0, 63]
+ * All methods will return -1 in the case of wrong nBit value
+ *
+ */
+public class BitOperations {
+/**
+ * 
+ * @param number any number
+ * @param nBit - number of bit
+ * @return value of bit with number nBit 
+ */
+	static public int getBitValue(long number, int nBit) {
+	int a = 10;
+	return -1;
+}
+/**
+ * 
+ * @param number - any number
+ * @param nBit - bit number
+ * @param value - true for 1, false for 0
+ * @return new number in which value of nBit'h bit will have a given value
+ */
+	static public long setBitValue(long number, int nBit, boolean value) {
+		//TODO
+		return -1;
+	}
+/**
+ * 
+ * @param number - any number
+ * @param nBit - bit number
+ * @return new number in which nBit'h will be reverted (old value - 1, new value - 0)
+ */
+	static public long revertBit(long number, int nBit) {
+		//TODO
+		return -1;
+	}
+}
--- src/java_introduction/PrimitivesTest.java
+++ src/java_introduction/PrimitivesTest.java
@@ -2,11 +2,13 @@ package java_introduction;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class PrimitivesTest {
 
 	@Test
+	@Disabled
 	void dataTypeTest() {
 		int b = 10;
 		short a = 20;
@@ -15,12 +17,12 @@ class PrimitivesTest {
 		byte b1 = (byte) 1000;
 		a = (short) l;
 		float f = 10.2f;
-		int b2 = 0xfffffff1;
-		
+		int b2 = 0xfffffff1;		
 		assertEquals(-15, b2);
-		
 	}
+		
 	@Test
+	@Disabled
 	void operatorsTest() {
 		int a = 10;
 		assertEquals(13, a + 3);
@@ -32,13 +34,40 @@ class PrimitivesTest {
 
 //Assumption: all three methods take number comprising of three digits
 	}
+	@Test
+	@Disabled
 	private int getFirstDigit(int number) {
 		return number / 100 % 10;
 	}
+	@Test
+	@Disabled
 	private int getSecondDigit(int number) {
 		return number / 10 % 10;
 	}
+	@Test
+	@Disabled
 	private int getThirdDigit(int number) {
 		return number % 10;
+	}
+	
+	
+	void getBitValue() {
+		long number = 0x3ab7f5; //001110101011011111110101
+		assertEquals(1, BitOperations.getBitValue(number, 5));
+		assertEquals(0, BitOperations.getBitValue(number, 11));
+		assertEquals(0, BitOperations.getBitValue(number, 1));
+		assertEquals(1, BitOperations.getBitValue(number, 2));
+	}
+	
+	void setBitValueTest() {
+		long number = 0x3ab7f5;
+		assertEquals(0x3ab7e5, BitOperations.setBitValue(number, 5, false));
+		assertEquals(0x3ab7f5, BitOperations.setBitValue(number, 5, true));
+	}
+	
+	void revertBitValueTest() {
+		long number = 0x3ab7f5;
+		assertEquals(0x3ab7e5, BitOperations.revertBit(number, 5));
+		assertEquals(0x3ab7f5, BitOperations.revertBit(number, 0));
 	}
 }
