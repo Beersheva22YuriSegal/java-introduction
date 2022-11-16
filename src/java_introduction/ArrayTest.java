5dcd9e357c0cf83dc3291574df2fe4fc059e809c
YuriSegal <yurisegal8@gmail.com>
2022-11-16 23:29:01 +0200

    CW#8 HW#8 1/2 + tests completed

--- src/java_introduction/ArrayTest.java
+++ src/java_introduction/ArrayTest.java
@@ -1,4 +1,5 @@
 package java_introduction;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Disabled;
@@ -7,24 +8,26 @@ import org.junit.jupiter.api.Test;
 public class ArrayTest {
 
 	@Test
+	@Disabled
 	void binarySearchLeftTest() {
-		int ar0[] = {1, 1, 2, 3, 4};
-		int ar1[] = {1, 2, 3, 4, 4};
-		int ar2[] = {1, 2, 3, 4, 4, 4, 4, 4, 4, 5, 6};
-		int ar3[] = {2, 2, 2, 4, 4, 4};
-		assertEquals(0, MyArrays.binarySearchLeft(ar0, 1));
-		assertEquals(3, MyArrays.binarySearchLeft(ar1, 4));
-		assertEquals(3, MyArrays.binarySearchLeft(ar2, 4));
-		assertEquals(-7, MyArrays.binarySearchLeft(ar3, 5));	
+		int ar[] = { 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4, 20, 40 };
+		assertEquals(-14, MyArrays.binarySearchLeft(ar, 3));
+		assertEquals(3, MyArrays.binarySearchLeft(ar, 2));
+		assertEquals(-1, MyArrays.binarySearchLeft(ar, 0));
+		assertEquals(13, MyArrays.binarySearch(ar, 4));
+		assertEquals(0, MyArrays.binarySearchLeft(ar, 1));
+		assertEquals(-16, MyArrays.binarySearchLeft(ar, 25));
+		assertEquals(-17, MyArrays.binarySearchLeft(ar, 45));
 	}
-	
+
 	@Test
+	@Disabled
 	void bubbleSortTest() {
 		int[] arr = new int[] { 2, -1, -3 };
 		int[] arr1 = new int[] { 10, 17, -4, 0, -18 };
 		int[] arr2 = new int[] { 1, 3, 1, 2, -6, -9 };
 		MyArrays.bubbleSort(arr);
-		assertArrayEquals(new int[] { -3, -1, 2 }, arr);	
+		assertArrayEquals(new int[] { -3, -1, 2 }, arr);
 		MyArrays.bubbleSort(arr1);
 		assertArrayEquals(new int[] { -18, -4, 0, 10, 17 }, arr1);
 		MyArrays.bubbleSort(arr2);
@@ -32,14 +35,15 @@ public class ArrayTest {
 	}
 
 	@Test
+	@Disabled
 	void isOneSwapTestFalse() {
 		int ar1[] = { 1, 2, 3, 10, -1, 5, 6 };
 		int ar2[] = { 1, 2, 3, 4, 5, 10 };
 		int ar3[] = { 5, 1, 2, 4, 6, 10 };
 		int ar4[] = { 1, 5, 2, 4, 3, 10 };
 		int ar5[] = { 1, 3, 2, 5, 4, 10, 8 };
-		int ar6[] = {1, 3, 20, 4, 5, 6, 10};
-		int ar7[] = {1, 3, 20, 4, 5, 11, 2};
+		int ar6[] = { 1, 3, 20, 4, 5, 6, 10 };
+		int ar7[] = { 1, 3, 20, 4, 5, 11, 2 };
 		assertFalse(MyArrays.isOneSwapForSorted(ar1));
 		assertFalse(MyArrays.isOneSwapForSorted(ar2));
 		assertFalse(MyArrays.isOneSwapForSorted(ar3));
@@ -48,8 +52,9 @@ public class ArrayTest {
 		assertFalse(MyArrays.isOneSwapForSorted(ar6));
 		assertFalse(MyArrays.isOneSwapForSorted(ar7));
 	}
-	
+
 	@Test
+	@Disabled
 	void isOneSwapTestTrue() {
 		int ar1[] = { 10, 2, 3, 4, 1 };
 		int ar2[] = { 1, 2, 4, 3, 5, 10 };
@@ -58,6 +63,8 @@ public class ArrayTest {
 		int ar5[] = { 1, 2, 3, 4, 10, 5 };
 		int ar6[] = { 2, 1, -3, 4, 5, 10 };
 		int ar7[] = { 3, 2, 1, 4, 5, 6 };
+		int ar8[] = { 1, 2, 2, 3, 3, 4, 4, 4, 3, 5 };
+		int ar9[] = { 1, 2, 4, 3, 3, 3, 5 };
 		assertTrue(MyArrays.isOneSwapForSorted(ar1));
 		assertTrue(MyArrays.isOneSwapForSorted(ar2));
 		assertTrue(MyArrays.isOneSwapForSorted(ar3));
@@ -65,5 +72,20 @@ public class ArrayTest {
 		assertTrue(MyArrays.isOneSwapForSorted(ar5));
 		assertTrue(MyArrays.isOneSwapForSorted(ar6));
 		assertTrue(MyArrays.isOneSwapForSorted(ar7));
-		}
+		assertTrue(MyArrays.isOneSwapForSorted(ar8));
+		assertTrue(MyArrays.isOneSwapForSorted(ar9));
+	}
+	@Test
+	void isSum2Test() {
+		assertTrue(MyArrays.isSum2(new short[] {1, 2, 3, 15, 35}, (short) 3));
+		assertTrue(MyArrays.isSum2(new short[] {1, 2, 3, 15, 35}, (short) 50));
+		assertTrue(MyArrays.isSum2(new short[] {1, 2, 3, 15, 35}, (short) 38));
+		assertTrue(MyArrays.isSum2(new short[] {1, 2, 3, 15, 35}, (short) 4));
+		assertTrue(MyArrays.isSum2(new short[] {1, 2, 2, 2, 35}, (short) 4));
+		
+		assertFalse(MyArrays.isSum2(new short[] {1, 2, 3, 15, 35}, (short) 10));
+		assertFalse(MyArrays.isSum2(new short[] {1, 2, 3, 15, 35}, (short) 20));
+		assertFalse(MyArrays.isSum2(new short[] {1, 2, 3, 15, 35}, (short) 30));
+		assertFalse(MyArrays.isSum2(new short[] {1, 2, 2, 2, 35}, (short) 6));
+	}
 }
--- src/java_introduction/MyArrays.java
+++ src/java_introduction/MyArrays.java
@@ -77,86 +77,98 @@ public class MyArrays {
 	}
 
 	public static int binarySearchLeft(int[] arraySorted, int number) {
-		int left = 0;
-		int right = arraySorted.length - 1;
-		int middle = right / 2;
-		int res = -1;
-		while (left <= right) {
-			if (number < arraySorted[middle]) {
-				right = middle - 1;
-			} else if (number > arraySorted[middle]) {
-				left = middle + 1;
+		int l = 0;
+		int r = arraySorted.length - 1;
+		int mid = arraySorted.length / 2;
+		while (l <= r && arraySorted[l] != number) {
+			if (number <= arraySorted[mid]) {
+				r = mid - 1;
 			} else {
-				res = middle;
-				right = middle - 1;
+				l = mid + 1;
 			}
-			middle = (left + right) / 2;
+			mid = (l + r) / 2;
 		}
-		if (res == -1) {
-			res = -left - 1;
-		}
-		return res;
+		return l < arraySorted.length && arraySorted[l] == number ? l : -l - 1;
 	}
 
 	public static void bubbleSort(int[] arr) {
-		while (moveGreaterRight(arr))
-			;
+		int uncheckedLength = arr.length;
+		do {
+			uncheckedLength = moveGreaterRight(arr, uncheckedLength - 1);
+		} while (uncheckedLength != 0);
 	}
 
-	private static boolean moveGreaterRight(int[] arr) {
-		boolean res = false;
-		for (int i = 0; i < arr.length - 1; i++) {
+	private static int moveGreaterRight(int[] arr, int length) {
+		int res = 0;
+		for (int i = 0; i < length; i++) {
 			if (arr[i] > arr[i + 1]) {
-				int temp = arr[i];
-				arr[i] = arr[i + 1];
-				arr[i + 1] = temp;
-				res = true;
+				res = i + 1;
+				swap(arr, i, i + 1);
 			}
 		}
 		return res;
 	}
 
 	public static boolean isOneSwapForSorted(int[] arr) {
-		int firstNum = 0;
-		int secondNum = 0;
-		int count = 0;
-		for (int k = 1; k < arr.length; k++) {
-			if (arr[k] < arr[k - 1]) {
-				count++;
-				if (firstNum == 0) {
-					firstNum = k;
+		int index1 = -1;
+		int index2 = -1;
+		int length = arr.length - 1;
+		int equaledCount = 0;
+		boolean res = true;
+		int i = 0;
+		while (i < length && res) {
+			if (arr[i] > arr[i + 1]) {
+				if (index1 == -1) {
+					index1 = i - equaledCount;
+					if (equaledCount > 0) {
+						index2 = i + 1;
+					}
+				} else if (index2 != -1) {
+					res = false;
 				} else {
-					secondNum = k;
+					index2 = i + 1;
 				}
+			} else if (arr[i] == arr[i + 1]) {
+				equaledCount++;
+			} else if (arr[i] < arr[i + 1]) {
+				if (equaledCount != 0 && index1 != -1 && index2 == -1 && arr[i] < arr[index1]) {
+					index2 = i;
+				}
+				equaledCount = 0;
 			}
+			i++;
 		}
-		if (count == 0) {
-			return false;
-		}
-		if (count > 2) {
-			return false;
-		}
-		if (count == 1) {
-			swap(arr, firstNum - 1, firstNum);
-		}
-		if (count == 2) {
-			swap(arr, firstNum - 1, secondNum);
-		}
-		return sorted(arr);
+		return index1 != -1 && res ? checkIndexes(arr, index1, index2) : false;
 	}
 
-	public static boolean sorted(int[] arr) {
-		for (int i = 1; i < arr.length; i++) {
-			if (arr[i] < arr[i - 1]) {
-				return false;
-			}
-		}
-		return true;
+	private static boolean checkIndexes(int[] arr, int index1, int index2) {
+		return index2 == -1 ? checkOneIndex(arr, index1) : checkTwoIndexes(arr, index1, index2);
 	}
 
-	private static void swap(int[] arr, int n, int m) {
-		int temp = arr[n];
-		arr[n] = arr[m];
-		arr[m] = temp;
+	private static boolean checkTwoIndexes(int[] arr, int index1, int index2) {
+		return (index2 == arr.length - 1 || arr[index1] <= arr[index2 + 1]) 
+				&& arr[index2] <= arr[index1 + 1]
+				&& (index1 == 0 || arr[index2] >= arr[index1 - 1]);
+	}
+
+	private static boolean checkOneIndex(int[] arr, int index) {
+		return (index == arr.length - 2 || arr[index] <= arr[index + 2])
+				&& (index == 0 || arr[index + 1] >= arr[index - 1]);
+	}
+	
+	private static void swap(int[] arr, int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+	
+	/**
+	 * 
+	 * @param array of short positive numbers
+	 * @param sum
+	 * @return true if array contains two numbers, sum of which equals a given sum
+	 */
+	public static boolean isSum2(short[] arr, short sum) {
+		return false;
 	}
 }
