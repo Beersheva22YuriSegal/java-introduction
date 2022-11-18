0445b0203aa099be2299153ba081f8ba04236ccc
YuriSegal <yurisegal8@gmail.com>
2022-11-18 19:02:25 +0200

    CW#9 objects

---
+++ src/java_introduction/Strings.java
@@ -0,0 +1,17 @@
+package java_introduction;
+
+public class Strings {
+	
+/**
+ * 
+ * @param str1
+ * @param str2
+ * @return true if str2 is anagram of str1
+ * complexity O[N] two passes
+ * 1. the same length
+ * 2. the same symbols just in different order
+ */
+	public static boolean isAnagram(String str1, String str2) {
+		return false;
+	}
+}
